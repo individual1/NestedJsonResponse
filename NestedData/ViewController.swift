433d2f5a8782715b1f2aa4f37d0a9e0b8c72afa5
Bhawna <bhawna.choudhary@wildnettechnologies.com>
2018-02-02 16:23:54 +0530

    Initial Commit

---
+++ NestedData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NestedData
+//
+//  Created by Bhawna on 02/02/18.
+//  Copyright © 2018 Bhawna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
